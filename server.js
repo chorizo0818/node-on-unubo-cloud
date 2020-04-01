cf2952e63adc93b1064a4d96f4f56e9e61eb8829
小屋 夕介 <89784@ad.exa-corp.co.jp>
2020-04-01 11:03:33 +0900

    more simplified.

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-require('dotenv').config();
-
+const app = require('express')();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-let express = require('express');
-let app = express();
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (request, response) {
-  response.sendFile(path.join(__dirname + '/views/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
