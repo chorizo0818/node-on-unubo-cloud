72a70b4323468c28022fa4ca10b4941b9a7eb4f5
小屋 夕介 <89784@ad.exa-corp.co.jp>
2020-04-01 11:23:06 +0900

    add requirements.

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-const app = require('express')();
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+// const httpsRedirect = require('parse-express-https-redirect'); // HTTPS に強制リダイレクトすべきか
+const httpRequest = require('request');
+const url = require('url');
+const mimelib = require('mimelib');
+const jschardet = require('jschardet');
+const iconv = require('iconv-lite');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const mongouri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.MONGOHOST+'?retryWrites=true';
+const mongoOptions = {useNewUrlParser:true, useUnifiedTopology:true };
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
